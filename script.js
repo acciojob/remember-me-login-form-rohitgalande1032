4cd9a7de82c97c88be177cc2bb2b7c97bc99b54a
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-29 16:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click",() => {
+	let username = document.getElementById("username");
+	let password = document.getElementById("password");
+	let checkbox = document.getElementById("checkbox");
+
+	
+	let userObj = {Username: username.value, Password: password.value}
+	console.log(userObj)
+	if(checkbox.checked) {
+		localStorage.setItem("userObj", JSON.stringify(userObj))
+	}else{
+		localStorage.removeItem("userObj");
+		alert("Logged in as.")
+	}
+	
+})
+
+if(localStorage.getItem("userObj")){
+	document.getElementById("form").innerHTML += `
+		<input onclick="showAlert()" type="submit" id="existing" value="Login as existing user.">
+	`
+}
+
+function showAlert() {
+	alert("Logged in as.")
+}
+
